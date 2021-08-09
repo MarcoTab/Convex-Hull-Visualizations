c6de4953b6ce704823718f91d5402e2b14f57f3f
Marco Tabacman <marco.tabacman@gmail.com>
2021-08-09 14:42:29 -0300

    Removed Chan's from list of algorithms, will add later when implemented.

--- src/convexhull/Tester.java
+++ src/convexhull/Tester.java
@@ -1,47 +1,99 @@
 package convexhull;
 import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.*;  
 
 @SuppressWarnings("unused")
-public class Tester {  
-	private JFrame f;
-	private ThisPanel panel;
-	private int size = 500;
+public class Tester {
 	
-	Tester() {  
-		f = new JFrame("Panel Example");    
-        panel=new ThisPanel();  
-        panel.setSize(size, size);
-        panel.setBackground(Color.WHITE); 
-        f.setSize(size,size+27);
-        f.setLayout(new GridBagLayout());  
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = 5;
-        c.gridheight = 40;
-        f.add(panel, c); 
-        f.setVisible(true);
-    }  
-    
-	
+	public static List<Point2D> pointMaker(int n) {
+		List<Point2D> l = new ArrayList<Point2D>();
+		Random rd = new Random();
+		for (int i = 0; i < n; i++) {
+			l.add(new Point2D(rd.nextFloat()*n, rd.nextFloat()*n));
+		}
+		return l;
+	}
 	
 	public static void main(String args[]) {  
-		new Tester();
-		Line2D line = new Line2D(180.0, 53.2, 14.9, 24.3);
-		System.out.println(MyPanel.toScale(line));
-		System.out.println(MyPanel.toScale(line.getStart()) + " -> " + MyPanel.toScale(line.getEnd()));
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input size > ");
+		int n = scanner.nextInt();
+		System.out.print("Reps > ");
+		int reps = scanner.nextInt();
+		System.out.print("What algorithm? <s, g, q, j, i, d> ");
+		char alg = scanner.next().charAt(0);
+		scanner.close();
+		
+		long [] times = new long[reps];
+		
+		for (int i = 0; i < reps; i++) {
+			System.out.println("Round " + (i+1) + " of " + reps);
+			List<Point2D> l = pointMaker(n);
+			
+			long time = 0;
+			
+			if (alg == 's') {
+				time = System.currentTimeMillis();
+				try{MyPanel.slowHull(l, null);} catch (Exception e) {e.printStackTrace();}
+				time = System.currentTimeMillis() - time;
+				
+			} else if (alg == 'g') {
+				time = System.currentTimeMillis();
+				try{MyPanel.grahamHull(l, null);} catch (Exception e) {e.printStackTrace();}
+				time = System.currentTimeMillis() - time;
+
+			} else if (alg == 'q') {
+				time = System.currentTimeMillis();
+				try{MyPanel.quickHull(l, null);} catch (Exception e) {e.printStackTrace();}
+				time = System.currentTimeMillis() - time;
+
+			} else if (alg == 'i') {
+				time = System.currentTimeMillis();
+				try{MyPanel.incrementalHull(l, null);} catch (Exception e) {e.printStackTrace();}
+				time = System.currentTimeMillis() - time;
+
+			} else if (alg == 'j') {
+				time = System.currentTimeMillis();
+				try{MyPanel.jarvisHull(l, null);} catch (Exception e) {e.printStackTrace();}
+				time = System.currentTimeMillis() - time;
+
+			} else if (alg == 'd') {
+				time = System.currentTimeMillis();
+				try{MyPanel.dncHull(l, null);} catch (Exception e) {e.printStackTrace();}
+				time = System.currentTimeMillis() - time;
+			}
+			
+			System.out.println("> " + time + "ms");
+			times[i] = time;
+		}
+		float sum = 0;
+		
+		for (long num : times) {
+			sum += num;
+		}
+		
+		sum /= reps;
+		FileWriter f = null;
+		try{
+		f = new FileWriter("time.dat");
+		} catch(IOException e) {e.printStackTrace();};
+		if (f != null)
+			try {
+				f.write("Performed " + reps + " multijoins on sets of size " + n + " w/ an average time of " + sum + "ms");
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		
 	}   
      
-    class ThisPanel extends JPanel {
-		private static final long serialVersionUID = 1L;
-
-		public void paintComponent(Graphics g) {
-    		super.paintComponent(g);
-    	}
-    	
-    	
-    }
         
 }  
